af5b22d46c29b70f4cc37a0e5e41a4e2d32bcd73
Boyce Codd <codd@ubuntu.(none)>
2013-03-16 11:15:22 -0700

    generate rails apk

---
+++ northwind/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Northwind::Application.config.secret_token = 'a7547aab9225c8b55113587488b907da1811ff0aeb4ddd3d3a080e92712b51410e4eaf69f509d87eb27620327d8bf18da85b047871888aa8d4669f2a40343fbf'
