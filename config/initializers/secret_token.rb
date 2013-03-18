3c952b0a62a5f42618b254c6513d8c542ea087ec
Mateusz Opala <mateuszjacekopala@gmail.com>
2013-03-18 12:52:52 +0100

    clean

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NorthwindApp::Application.config.secret_token = 'c4e3885af6b4bc6112ab77b6077fa844379fd0fe3f5c1629357575ac4a49042e7ee4c8b3ba65afca1eb5e1cf714bcc739bef233fee54e9069e3b031db05d7b61'
