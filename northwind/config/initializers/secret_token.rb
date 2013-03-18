075441ec8cf76f4a5bec267c7988b7421128098c
Boyce Codd <codd@ubuntu.(none)>
2013-03-18 01:47:21 -0700

    delete

--- northwind/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Northwind::Application.config.secret_token = 'ffdb8a1d9655b1aa04d3850d194cc917506af1455ce8446aa4943d0bfe6a5f9abad3e6032c9172da18bca26c0d541d6b451e3875fb5528e4f41ca81eaa1c0a37'
